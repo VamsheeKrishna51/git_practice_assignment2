7f0ebe4ef7ad41f9d9090e821073259f29eb4497
Shiva3108 <shivakumarlonka9@gmail.com>
2023-02-22 22:28:02 +0530

    code fixed

--- primenumber.js
+++ primenumber.js
@@ -1,11 +1,11 @@
 let number= 13;
 let count = 0;
-for(let i=1;i<=number;i++){
- if(num%1==0){
+for(let i=2;i<=Math.sqrt(number);i++){
+ if(num%i==0){
 count++;
 }
 }
-if(count==1){
+if(count==0){
 
 console.log("prime");
 }
